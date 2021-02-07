5f64147817039f1ca262f40731fbac24a70fb0ec
kyi <nalabi@gmail.com>
2021-02-07 16:14:33 +0900

    Initial commit

---
+++ app/src/main/java/com/yong/frame/MainActivity.java
@@ -0,0 +1,62 @@
+package com.yong.frame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1) ;
+        button1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeView(0) ;
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2) ;
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeView(1) ;
+            }
+        });
+    }
+
+    private void changeView(int index) {
+        // LayoutInflater 초기화.
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        FrameLayout frame = (FrameLayout) findViewById(R.id.frame) ;
+        if (frame.getChildCount() > 0) {
+            // FrameLayout에서 뷰 삭제.
+            frame.removeViewAt(0);
+        }
+
+        // XML에 작성된 레이아웃을 View 객체로 변환.
+        View view = null ;
+        switch (index) {
+            case 0 :
+                view = inflater.inflate(R.layout.page1, frame, false) ;
+                break ;
+            case 1 :
+                view = inflater.inflate(R.layout.page2, frame, false) ;
+                break ;
+        }
+
+        // FrameLayout에 뷰 추가.
+        if (view != null) {
+            frame.addView(view) ;
+        }
+    }
+}
